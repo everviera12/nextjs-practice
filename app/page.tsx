4a07bc0e4cd05c6ee78e6033982274ce0acb260c
everviera12 <everisc2@gmail.com>
2026-02-06 12:39:35 -0600

    auth process practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
-import Image from 'next/image';
+'use client'
+
+import { useState } from "react";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [name, setName] = useState<string>('')
+
+  const handleLogin = (userName: string) => {
+    setIsLoggedIn(true)
+    setName(userName)
+    console.log(userName);
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+    setName('')
+    console.log('Logout active');
+  }
+
   return (
     <div className="h-screen p-6 ">
-      <h1 className="mt-6 font-bold text-white">Home Page</h1>
+      <button
+        className="bg-sky-100 text-sky-700 px-6 py-2 rounded-full cursor-pointer"
+        onClick={() => isLoggedIn ? handleLogout() : handleLogin('Admin')}
+      >
+        {isLoggedIn ? 'Logout' : 'Login'}
+      </button>
 
-      <div className="bg-white">
-        <Image src={'/directorio.svg'} fill alt="Directorio" />
-      </div>
-
+      <h1 className="mt-6 font-bold text-white">
+        Welcome <span className="text-green-400 font-bold"> {name} </span>
+      </h1>
     </div>
   );
 }
