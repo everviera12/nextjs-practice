2361e5fdfd998470472ed9955dc87e216dd6bfc5
everviera12 <everisc2@gmail.com>
2026-02-09 17:07:10 -0600

    change ant design library for material ui

--- app/_components/_ui/header.tsx
+++ app/_components/_ui/header.tsx
@@ -1,40 +1,21 @@
 "use client";
 
-import LinkNext from "next/link";
-import { Button, Menu, MenuProps } from "antd";
+import { Button } from "@mui/material";
 import { useAuthContext } from "../auth-context";
-
-type MenuItem = Required<MenuProps>["items"][number];
-
-// Custom Link
-const Link = () => {
-  return <LinkNext href="/">Home</LinkNext>;
-};
-
-const items: MenuItem[] = [
-  {
-    key: "home",
-    label: (
-        <Link />
-    )
-  },
-];
 
 export default function Header() {
   const { handleLogout, handleLogin, isLoggedIn } = useAuthContext();
 
-  const isLoggedInTrue = "bg-green-400 text-green-700";
-  const isLoggedInFalse = "bg-sky-100 text-sky-700";
+  const isLoggedInTrue = "bg-green-100! text-green-900!";
+  const isLoggedInFalse = "bg-sky-100! text-sky-900!";
 
   return (
     <header>
       <nav className="">
-        <Menu key={"nav-menu"} mode="horizontal" items={items} />
-
         <Button
-          type="primary"
-          className={`px-6 py-2 rounded-full cursor-pointer ${isLoggedIn ? isLoggedInTrue : isLoggedInFalse}`}
+          className={`font-bold! ${isLoggedIn ? isLoggedInTrue : isLoggedInFalse}`}
           onClick={() => (isLoggedIn ? handleLogout() : handleLogin("Admin"))}
+          variant="contained"
         >
           {isLoggedIn ? "Logout" : "Login"}
         </Button>
